795f8a56a5204221a2ba2659042b80800d44fe77
bigdogmat <bigdogmat123@gmail.com>
2015-09-20 23:45:03 -0400

    Changed send function
    PlayerInitialSpawn and PlayerDisconnected don't have two player arguments. No need to loop through every player when you can just broadcast.

--- lua/autorun/server/sv_data.lua
+++ lua/autorun/server/sv_data.lua
@@ -1,19 +1,13 @@
-hook.Add("PlayerInitialSpawn","lastjoin",function(pl, ply) 
-	local lastjoin = pl:GetPData("lastjoined") 
-	if lastjoin == nil then
-		for _,ply in pairs(player.GetHumans()) do 
-			ply:PlayerMsg(Color(80,251,80),"● ",Color(255,255,255),pl:Nick().."("..pl:SteamID()..")".." has ",Color(80, 251, 80),"joined ",Color(255,255,255),"for the first time welcome!")--ply:ChatPrint(ply:Nick().."("..ply:SteamID()..")".."has joined for the first time welcome!") 
-		end
-	elseif lastjoin then
-		for _,ply in pairs(player.GetHumans()) do
-		ply:PlayerMsg(Color(80, 251, 80),"● ",Color(255,255,255),pl:Nick().."("..pl:SteamID()..")".." has ",Color(80, 251, 80),"joined\n",Color(80, 80, 80),"● ",Color(255,255,255),"Last seen "..lastjoin)--ply:ChatPrint(ply:Nick().."("..ply:SteamID()..")".." has joined\nLast seen "..lastjoin)
-		end
+hook.Add("PlayerInitialSpawn","lastjoin",function(ply) 
+	local lastjoin = ply:GetPData("lastjoined", "") 
+	if lastjoin then
+		BroadcastMsg(Color(80, 251, 80), "● ", Color(255,255,255), ply:Nick() .. "(" .. ply:SteamID() .. ")" .. " has ", Color(80, 251, 80), "joined\n", Color(80, 80, 80), "● ", Color(255,255,255), "Last seen " .. lastjoin)
+	else
+		BroadcastMsg(Color(80,251,80), "● ", Color(255,255,255), ply:Nick() .. "(" .. ply:SteamID() .. ")" .. " has ", Color(80, 251, 80), "joined ", Color(255,255,255), "for the first time welcome!")
 	end
 end)
 
-hook.Add("PlayerDisconnected","lastjoinsave",function(pl, ply)
-		pl:SetPData("lastjoined",os.date("%d/%m/%Y %I:%M%p", os.time()))
-	for _,ply in pairs(player.GetHumans()) do 
-		ply:PlayerMsg(Color(255, 51, 51, 255),"● ",Color(255,255,255),pl:Nick().."("..pl:SteamID()..")".." has ",Color(255, 51, 51, 255),"left ",Color(255,255,255),"the server.")
-	end
+hook.Add("PlayerDisconnected","lastjoinsave",function(ply)
+	ply:SetPData("lastjoined",os.date("%d/%m/%Y %I:%M%p", os.time()))
+	BroadcastMsg(Color(255, 51, 51, 255), "● ", Color(255,255,255), ply:Nick() .. "(" .. ply:SteamID() .. ")" .. " has ", Color(255, 51, 51, 255), "left ", Color(255,255,255), "the server.")
 end)
